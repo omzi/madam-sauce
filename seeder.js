c11584fb5fb66f11eacc8e8ddf1be6644a590d13
omzi <obiohaomezibe@gmail.com>
2020-10-14 19:11:31 +0100

    Created database seeder for the 'foods' collection🌱

---
+++ seeder.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env variables
+dotenv.config({ path: './config/.env' })
+
+// Load models
+const Food = require('./models/Food');
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+
+// Read JSON files
+const foods = JSON.parse(fs.readFileSync(`${__dirname}/.data/foods.json`, 'utf-8'))
+
+// Import into database
+const importData = async () => {
+  try {
+    await Food.create(foods);
+
+    console.log(' ↓ | Data imported... '.black.bgGreen.bold.inverse);
+    process.exit(1)
+  } catch (error) {
+    console.log(`✖ | Error: ${error}`.red.bold)
+  }
+}
+
+// Delete from database
+const deleteData = async () => {
+  try {
+    await Food.deleteMany();
+
+    console.log(' ↶ | Data destroyed... '.black.bgRed.bold.inverse);
+    process.exit(1)
+  } catch (error) {
+    console.log(`✖ | Error: ${error}`.red.bold)
+  }
+}
+
+switch (process.argv[2]) {
+  case '-i':
+    importData()
+    break;
+  case '-d':
+    deleteData()
+    break;
+  default:
+    console.log(' ☄ | Unknown flag. Possible flags are -i (import) & -d (destroy) '.blue.bold.inverse);
+    process.exit(1)
+}
