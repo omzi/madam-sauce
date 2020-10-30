c6d5ec4cab05491b9d02fccf8f63b232f00a934a
omzi <obiohaomezibe@gmail.com>
2020-10-30 09:55:41 +0100

    Fixed start script issue

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 });
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'))
 
 // Rate limit options
 const limiter = rateLimit({
