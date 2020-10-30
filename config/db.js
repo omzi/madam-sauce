aed1d90ea1b66f9b3fd984aa4238b014a8b6c448
omzi <obiohaomezibe@gmail.com>
2020-10-30 10:07:50 +0100

    Revert "Feat📌: Completed the front-end, made it dynamic & connected it to DB🤩🎉"
    This reverts commit 330fa992e7e17fffa9ecbd22a5e7d9f99d035e98.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
-  const { connection } = await mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD_URI : process.env.MONGO_URI, {
+  const { connection } = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin'],
+    enum: ['user'],
     default: 'user'
   },
   password: {
